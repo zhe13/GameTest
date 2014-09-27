280cc4aa018a61e0e08900aa4a4e106c63adc0ca
zhe13 <lovewutianzhe@vip.qq.com>
2014-09-27 13:27:50 +0800

    How to use
    并不很清楚怎么上传=-=

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject ball;
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = ball.transform.position + offset;
+	}
+}
---
+++ Assets/Script/MoveController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public float speed;
+	void FixedUpdate () {
+		float moveHor = Input.GetAxis ("Horizontal");
+		float moveVer = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHor, 0, moveVer);
+		rigidbody.AddForce (movement * speed *Time.deltaTime);
+		//为了确保帧独立，与delatetime相乘
+	}
+}
