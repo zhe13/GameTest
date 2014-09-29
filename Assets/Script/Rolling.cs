3d464f9943ad7edc001a70e0465eaddc98b526b8
呈秀波 <wutianzhe123@gmail.com>
2014-09-29 18:05:21 +0800

    第二次更新
    添加了墙以及四周的小方块

---
+++ Assets/Script/Rolling.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rolling : MonoBehaviour {
+	
+	//因为没有涉及到力，所以直接使用update
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		//为了确保平滑和帧独立所以x上deltaTime
+	}
+}
